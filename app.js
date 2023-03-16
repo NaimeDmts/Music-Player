5526b1edcf3bae0575771c94238cfc442f985319
NaimeDmts <naime.dmts@gmail.com>
2023-03-16 12:00:41 +0300

    3- Music Player - Controls

--- app.js
+++ app.js
@@ -22,5 +22,37 @@ function displayMusic(music){
 }
 
 play.addEventListener('click', () =>{
-    audio.play();
+
+    const isMusicPlay = container.classList.contains("playing");
+    isMusicPlay ? pauseMusic() : playMusic();
+});
+prev.addEventListener("click", () =>{
+    prevMusic();
+});
+function prevMusic(){
+    player.previous();
+    let music = player.getMusic();
+    displayMusic(music);
+    playMusic();
+
+}
+next.addEventListener("click", () =>{
+    nextMusic();
+    let music = player.getMusic();
+    displayMusic(music);
+    playMusic();
 })
+function nextMusic(){
+    player.next();
+}
+
+function pauseMusic(){
+    container.classList.remove("playing");
+    play.classList = "fa-solid fa-play";
+    audio.pause();
+}
+function playMusic(){
+    container.classList.add("playing");
+    play.classList = "fa-solid fa-pause";
+    audio.play();
+}
